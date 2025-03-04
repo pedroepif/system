7d14a3b1a5d9c0f08abf625d990aa42269e45566
Pedro Epifanio <pedroepifanio@MacBook-Air-de-Pedro.local>
2025-03-04 10:04:38 -0300

    fix: Adjusting bodySizeLimit

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,11 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 const nextConfig: NextConfig = {
   output: "standalone",
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "50mb",
+    },
+  },
 };
 
 export default withNextIntl(nextConfig);
